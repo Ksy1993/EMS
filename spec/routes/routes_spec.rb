30614ffab2484bd939da2db8fc0a85964479f10d
Kalyan Singh <kalyansingh6030@gmail.com>
2020-04-01 19:46:21 +0530

    changes in routes spec

--- spec/routes/routes_spec.rb
+++ spec/routes/routes_spec.rb
@@ -14,5 +14,11 @@ RSpec.describe EventsController, type: :routing do
   it 'routes to #new' do
    expect(get: '/events/new').to route_to('events#new')
   end
+   it 'routes to #edit' do
+   expect(get: '/events/edit/1').to route_to('events#edit', id: '1')
+  end
+  it 'routes to #destroy' do
+   expect(get: '/events/destroy/1').to route_to('events#destroy', id: '1')
+  end
 end
 end
