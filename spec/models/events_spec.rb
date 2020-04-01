5495cece2dc4a347174c801f2f392d24bc6634ca
Kalyan Singh <kalyansingh6030@gmail.com>
2020-04-01 19:45:29 +0530

    changes in model specs

--- spec/models/events_spec.rb
+++ spec/models/events_spec.rb
@@ -9,3 +9,35 @@ RSpec.describe Event, type: :model do
       it { should belong_to(:user) }
     end
 end
+
+
+require 'rails_helper'
+RSpec.describe Event, type: :model do
+subject { Event.new( title: "test20", start: "2020-03-31", end: "2020-04-02", user_id: 1)}
+before { subject.save}
+it "title should be present" do
+subject.title = nil
+expect(subject).to_not be_valid
+end
+it "start date should be present" do
+subject.start = nil
+expect(subject).to_not be_valid
+end
+it "end date should be present" do
+subject.end = nil
+expect(subject).to_not be_valid
+end
+it "user id should be present" do
+subject.user_id = nil
+expect(subject).to_not be_valid
+end
+it "title should not be too long" do
+subject.title = "a"
+expect(subject).to_not be_valid
+end
+it "title should not be too short" do
+subject.title = "a"
+expect(subject).to_not be_valid
+end
+
+end
