8c5be79ef2b8a18a9866f622cdd71292500e5ea1
Kalyan Singh <kalyansingh6030@gmail.com>
2020-04-01 17:47:22 +0530

    create new model rspec

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -6,3 +6,13 @@ RSpec.describe User, type: :model do
       it { should have_many(:events) }
     end
 end
+
+RSpec.describe User, type: :model do
+  it "is valid with a email and password" do
+    user = User.new(
+      email:      "ajay123@gmail.com",
+      password:   "ajay@123"
+    )
+    expect(user).to be_valid
+  end
+end
